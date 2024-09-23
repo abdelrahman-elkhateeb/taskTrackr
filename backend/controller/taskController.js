d8757d29ce7a53056a992556c1dc781864aa2470
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-09-23 18:03:45 +0300

    finalize crud operations

--- backend/controller/taskController.js
+++ backend/controller/taskController.js
@@ -1,6 +1,9 @@
-export const getTasks = async (req, res) => {
+import Task from "../model/taskModel";
+import mongoose from "mongoose";
+
+export const getTasks = async (res) => {
   try {
-    const tasks = await tasks.find({});
+    const tasks = await Task.find({});
     res.status(200).json({ success: true, data: tasks });
   } catch (err) {
     console.error(err.message);
@@ -8,7 +11,56 @@ export const getTasks = async (req, res) => {
   }
 };
 
-export const createTask = async (req,res)=>{
+export const createTask = async (req, res) => {
   const task = req.body;
-  if(!)
-}
+  if (!description || !priority) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please fill in all" });
+  }
+  const newTask = new Task(task);
+  try {
+    const savedTask = await newTask.save();
+    res.status(201).json({ success: true, data: savedTask });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "error in creating task" });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const taskData = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ success: false, message: "Invalid ID" });
+  }
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(id, taskData, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedTask });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ success: false, message: "server Error" });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ success: false, message: "Invalid ID" });
+  }
+  try {
+    const deletedTask = await Task.findByIdAndDelete(id);
+    if (!deletedTask) {
+      return res
+        .status(404)
+        .json({ success: false, message: "task not found" });
+    }
+    res
+      .status(200)
+      .json({ success: true, message: "task deleted successfully" });
+  } catch (err) {
+    console.log("error in deleting task:", err.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
--- backend/model/taskModel.js
+++ backend/model/taskModel.js
@@ -1,25 +1,28 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const taskSchema = new mongoose.Schema({
+const taskSchema = new mongoose.Schema(
+  {
     description: {
-        type: String,
-        required: [true, "Task description is required"],
+      type: String,
+      required: [true, "Task description is required"],
     },
     priority: {
-        type: String,
-        enum: ['low', 'medium', 'high'],
-        required: [true, "Task priority is required"],
+      type: String,
+      enum: ["low", "medium", "high"],
+      required: [true, "Task priority is required"],
     },
     completed: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     dueDate: {
-        type: Date,
-        required: [true, "Due date is required"],
-    }
-}, { timestamps: true });
+      type: Date,
+      required: [true, "Due date is required"],
+    },
+  },
+  { timestamps: true },
+);
 
-const Task = mongoose.model('Task', taskSchema);
+const Task = mongoose.model("Task", taskSchema);
 
 export default Task;
