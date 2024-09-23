c17d23a6ac7cb73c47d538ac3b327c89ffa4ed09
abdelrahman elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-09-23 19:19:35 +0300

    Update taskRoute.js

--- backend/routers/taskRoute.js
+++ backend/routers/taskRoute.js
@@ -3,15 +3,9 @@ import {
   getTasks,
   createTask,
   deleteTask,
-<<<<<<< HEAD
   updateTask } from "../controllers/taskController.js";
 const router = express.Router();
 
-
-=======
-  updateTask,
-} from "../controllers/taskController.js";
->>>>>>> 6dae81f65fd539bf1adbc9eb836fe60dc88d7838
 
 router.get("/", getTasks).post("/", createTask);
 router.delete("/:id", deleteTask).put("/:id", updateTask);
