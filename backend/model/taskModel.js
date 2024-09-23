09cfc6fe5756160b5604ed103092e6827b29bca8
AhmedAboRaya <ahmedaboraya399@gmail.com>
2024-09-23 17:41:55 +0300

    create schema for user and task

---
+++ backend/model/taskModel.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: [true, "Task description is required"],
+    },
+    priority: {
+        type: String,
+        enum: ['low', 'medium', 'high'],
+        required: [true, "Task priority is required"],
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+    dueDate: {
+        type: Date,
+        required: [true, "Due date is required"],
+    }
+}, { timestamps: true });
+
+const Task = mongoose.model('Task', taskSchema);
+
+export default Task;
