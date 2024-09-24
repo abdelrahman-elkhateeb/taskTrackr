b94704d5657071806374d624a974e3faf8fc47f4
Mahmoud Saaed Romeh <123378237+Engromeh@users.noreply.github.com>
2024-09-24 18:39:48 +0300

    log out

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -123,7 +123,14 @@ const Navbar = () => {
                 >
                   Services
                 </NavLink>
+                
               </li>
+              <li><NavLink
+                  to='/login'
+                  className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+               logout
+                </NavLink></li>
             </ul>
           </div>
         </div>
--- client/src/pages/Singup.jsx
+++ client/src/pages/Singup.jsx
@@ -1,21 +1,19 @@
-import { useForm } from "react-hook-form"
-import {  useNavigate } from "react-router-dom"
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const Singup = () => {
-const navgite =useNavigate()
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-      } = useForm()
-      const onSubmit = (data) => {
-        console.log(data)
-        navgite('/login')
-      }
-      
+  const navgite = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    navgite("/login");
+  };
+
   return (
-
-
     <>
       <div className="flex font-poppins items-center justify-center">
         <div className="h-screen w-screen flex justify-center items-center dark:bg-gray-900">
@@ -26,12 +24,15 @@ const navgite =useNavigate()
             >
               <div className="border-[20px] border-transparent rounded-[20px] dark:bg-gray-900 bg-white shadow-lg xl:p-10 2xl:p-10 lg:p-10 md:p-10 sm:p-2 m-2">
                 <h1 className="pt-8 pb-6 font-bold dark:text-gray-400 text-5xl text-center cursor-default">
-              Singup
+                  Singup
                 </h1>
-                <form action="#" method="post" className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-
-
-                <div>
+                <form
+                  action="#"
+                  method="post"
+                  className="space-y-4"
+                  onSubmit={handleSubmit(onSubmit)}
+                >
+                  <div>
                     <label
                       htmlFor="text"
                       className="mb-2  dark:text-gray-400 text-lg"
@@ -43,31 +44,27 @@ const navgite =useNavigate()
                       className="border p-3 dark:bg-indigo-700 dark:text-gray-300  dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
                       type="Text"
                       placeholder="Text"
-                      {...register("firstname",{ required: true })}
-                  
-                  />
+                      {...register("firstname", { required: true })}
+                    />
                     {errors.firstname && <span>This field is required</span>}
                   </div>
-
 
                   <div>
                     <label
                       htmlFor="text"
                       className="mb-2  dark:text-gray-400 text-lg"
                     >
-                    Last Name
+                      Last Name
                     </label>
                     <input
                       id="Text"
                       className="border p-3 dark:bg-indigo-700 dark:text-gray-300  dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
                       type="Text"
                       placeholder="Text"
-                      {...register("lastname",{ required: true })}
-                  
-                  />
+                      {...register("lastname", { required: true })}
+                    />
                     {errors.lastname && <span>This field is required</span>}
                   </div>
-
 
                   <div>
                     <label
@@ -81,10 +78,9 @@ const navgite =useNavigate()
                       className="border p-3 dark:bg-indigo-700 dark:text-gray-300  dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
                       type="email"
                       placeholder="Email"
-                      {...register("email",{ required: true })}
-                  
+                      {...register("email", { required: true })}
                     />
-                      {errors.email && <span>This field is required</span>}
+                    {errors.email && <span>This field is required</span>}
                   </div>
                   <div>
                     <label
@@ -98,9 +94,8 @@ const navgite =useNavigate()
                       className="border p-3 shadow-md dark:bg-indigo-700 dark:text-gray-300  dark:border-gray-700 placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
                       type="password"
                       placeholder="Password"
-                      {...register("password",{ required: true })}
-                  
-                  />
+                      {...register("password", { required: true })}
+                    />
                     {errors.password && <span>This field is required</span>}
                   </div>
                   <div>
@@ -115,37 +110,25 @@ const navgite =useNavigate()
                       className="border p-3 shadow-md dark:bg-indigo-700 dark:text-gray-300  dark:border-gray-700 placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
                       type="password"
                       placeholder="Password"
-                      {...register("password",{ required: true })}
-                  
-                  />
+                      {...register("password", { required: true })}
+                    />
                     {errors.password && <span>This field is required</span>}
                   </div>
-                  
+
                   <button
                     className="bg-gradient-to-r dark:text-gray-300 from-blue-500 to-purple-500 shadow-lg mt-6 p-2 text-white rounded-lg w-full hover:scale-105 hover:from-purple-500 hover:to-blue-500 transition duration-300 ease-in-out block text-center"
                     type="submit"
-                    
                   >
                     Singup
                   </button>
                 </form>
-                
-
-                 
-                 
-                </div>
-
-           
-                      
-               
-                </div>
               </div>
             </div>
           </div>
-     
-    
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Singup
+export default Singup;
