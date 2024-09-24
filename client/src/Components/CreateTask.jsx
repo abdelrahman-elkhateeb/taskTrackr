e819de0f6887c5683ab3eeeab84f9a845d0a27ce
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2024-09-24 06:31:19 +0300

    modal form for new task

--- client/src/Components/CreateTask.jsx
+++ client/src/Components/CreateTask.jsx
@@ -1,11 +1,102 @@
+import { useState } from "react";
+import Modal from "./Modal";
+
 const CreateTask = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // handlers
+  const onToggleModal = () => setIsModalOpen(!isModalOpen);
   return (
     <div className="my-6">
       <button
         className={`bg-purple-700 py-2 px-4  rounded-lg font-medium text-white `}
+        onClick={onToggleModal}
       >
         Add New Task
       </button>
+      <Modal
+        isOpen={isModalOpen}
+        closeModal={onToggleModal}
+        title={"Add New Task"}
+      >
+        <form className="space-y-2 mt-3">
+          <div className="flex flex-col">
+            <label
+              htmlFor={"title"}
+              className="mb-2 font-medium text-sm text-gray-700"
+            >
+              Title
+            </label>
+            <input
+              className="border-[1px] rounded-lg shadow-md w-full  border-gray-300 focus:border-indigo-500 focus:outline-none focus:ring-1 
+        focus:ring-indigo-500 p-3 text-md
+        "
+              id="title"
+              type="text"
+              placeholder="Task title"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor={"description"}
+              className="mb-2 font-medium text-sm text-gray-700"
+            >
+              Description
+            </label>
+            <textarea
+              className="border-[1px] rounded-lg shadow-md w-full  border-gray-300 focus:border-indigo-500 focus:outline-none focus:ring-1 
+        focus:ring-indigo-500 p-3 text-md
+        "
+              id="description"
+              placeholder="Task description"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor={"date"}
+              className="mb-2 font-medium text-sm text-gray-700"
+            >
+              Due Date
+            </label>
+            <input
+              className="border-[1px] rounded-lg shadow-md w-full  border-gray-300 focus:border-indigo-500 focus:outline-none focus:ring-1 
+        focus:ring-indigo-500 p-3 text-md
+        "
+              id="date"
+              type="date"
+              placeholder="Task title"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor={"priority"}
+              className="mb-2 font-medium text-sm text-gray-700"
+            >
+              Priority
+            </label>
+            <select
+              id="priority"
+              className="border-[1px] rounded-lg shadow-md w-full  border-gray-300 focus:border-indigo-500 focus:outline-none focus:ring-1 
+              focus:ring-indigo-500 p-3 text-md
+              "
+            >
+              <option selected disabled>
+                Choose a Priority
+              </option>
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
+          </div>
+          <div className="flex items-center space-x-3">
+            <button className="w-full p-2 rounded-lg font-medium text-white bg-indigo-600 hover:bg-indigo-800">
+              Submit
+            </button>
+            <button className="w-full p-2 rounded-lg font-medium text-white bg-red-600 hover:bg-red-800" type='reset' onClick={onToggleModal}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 };
--- client/src/Components/Modal.jsx
+++ client/src/Components/Modal.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/prop-types */
-import { Dialog, Transition } from "@headlessui/react";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
 import { Fragment } from "react";
-export default function Modal({
-  isOpen,
-  closeModal,
-  children,
-  title,
-  description,
-}) {
+export default function Modal({ isOpen, closeModal, title, children }) {
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
           <div className="fixed inset-0 backdrop-blur-sm" aria-hidden="true" />
-          <Transition.Child
+          <TransitionChild
             as={Fragment}
             enter="ease-out duration-300"
             enterFrom="opacity-0"
@@ -23,11 +23,11 @@ export default function Modal({
             leaveTo="opacity-0"
           >
             <div className="fixed inset-0 bg-black/25" />
-          </Transition.Child>
+          </TransitionChild>
 
           <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
+          <div className="flex justify-center p-4 text-center mt-10">
+              <TransitionChild
                 as={Fragment}
                 enter="ease-out duration-300"
                 enterFrom="opacity-0 scale-95"
@@ -36,19 +36,16 @@ export default function Modal({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
+                <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <DialogTitle
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
                   >
                     {title}
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">{description}</p>
-                  </div>
+                  </DialogTitle>
                   {children}
-                </Dialog.Panel>
-              </Transition.Child>
+                </DialogPanel>
+              </TransitionChild>
             </div>
           </div>
         </Dialog>
