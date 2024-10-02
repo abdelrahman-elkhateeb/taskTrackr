aeb1c797ebc9b5598787175d4d203fc068b4c4ab
abdelrahman elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-10-02 21:19:06 +0300

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -114,6 +114,12 @@ const getUser = async (req, res) => {
   const { userId } = req.params;
 
   try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
     res.status(200).json({ user });
   } catch (error) {
     res.status(500).json({ message: error.message });
