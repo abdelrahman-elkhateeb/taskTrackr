7358c3ebe429f5b99ffff674e2f0321cebb1ba8f
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2024-10-05 22:29:44 +0300

    completed task checkbox

--- frontend/src/features/tasks/TaskCard.jsx
+++ frontend/src/features/tasks/TaskCard.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { MdDelete, MdEdit } from "react-icons/md";
+import { useUpdateTaskMutation } from "./tasksApi";
 
 const TaskCard = ({
   title,
@@ -11,6 +12,14 @@ const TaskCard = ({
   setTaskIdToDelete,
   setTaskToEdit,
 }) => {
+  const [updateTask] = useUpdateTaskMutation();
+
+  const onChangeCompleted = (e) => {
+    updateTask({
+      id,
+      body: { completed: e.target.checked },
+    });
+  };
   return (
     <>
       <div className="card bg-gray-700 max-w-sm shadow-xl">
@@ -36,35 +45,48 @@ const TaskCard = ({
             </p>
             <span
               className={`${
-                completed ? "bg-indigo-600" : "bg-blue-700"
+                completed ? "bg-cyan-600" : "bg-blue-700"
               } text-white py-1 px-4 rounded-full`}
             >
               {completed ? "Completed" : "In Progress"}
             </span>
           </div>
-          <div className="card-actions ">
-            <label
-              htmlFor="modal-update"
-              className="bg-teal-400 hover:bg-teal-600 text-gray-700 p-2 rounded-md"
-              onClick={() =>
-                setTaskToEdit({
-                  id,
-                  title,
-                  priority,
-                  description,
-                  dueDate,
-                })
-              }
-            >
-              <MdEdit />
-            </label>
-            <label
-              htmlFor="alert"
-              className=" bg-red-400 hover:bg-red-600 text-gray-700 p-2 rounded-md"
-              onClick={() => setTaskIdToDelete(id)}
-            >
-              <MdDelete />
-            </label>
+          <div className="flex justify-between items-center">
+            <div className="card-actions ">
+              <label
+                htmlFor="modal-update"
+                className="bg-teal-400 hover:bg-teal-600 text-gray-700 p-2 rounded-md cursor-pointer"
+                onClick={() =>
+                  setTaskToEdit({
+                    id,
+                    title,
+                    priority,
+                    description,
+                    dueDate,
+                  })
+                }
+              >
+                <MdEdit />
+              </label>
+              <label
+                htmlFor="alert"
+                className=" bg-red-400 hover:bg-red-600 text-gray-700 p-2 rounded-md cursor-pointer"
+                onClick={() => setTaskIdToDelete(id)}
+              >
+                <MdDelete />
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <span className="label-text mr-4 font-black">Completed</span>
+                <input
+                  type="checkbox"
+                  checked={completed}
+                  className="checkbox"
+                  onChange={onChangeCompleted}
+                />
+              </label>
+            </div>
           </div>
         </div>
       </div>
