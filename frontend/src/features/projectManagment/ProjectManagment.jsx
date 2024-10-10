8c77285c862887dc7703093068490fd664f5848d
abdelrahman elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-10-10 15:23:07 +0300

    fix project management route navbar

--- frontend/src/features/projectManagment/ProjectManagment.jsx
+++ frontend/src/features/projectManagment/ProjectManagment.jsx
@@ -1,5 +1,7 @@
 function ProjectManagement() {
-  return <div></div>;
+  return (
+    <section className="container mx-auto px-4">project Management</section>
+  );
 }
 
 export default ProjectManagement;
